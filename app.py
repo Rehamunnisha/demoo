9177bc3c9bda3147550d77f4f5f1a5921372abd1
S.K.R <59757127+Rehamunnisha@users.noreply.github.com>
2022-10-03 07:02:45 +0000

    i commit the code

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "hello, word"
+  return "Hello, World!"
 
-if __name__=="__main__":
+
+if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
